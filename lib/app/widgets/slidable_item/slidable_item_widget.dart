820494eb3dfe359e4a11c96334ee3d4b11d7fb09
José Israel Gutierrez Salazar <israelgs83@gmail.com>
2022-09-11 22:51:16 -0500

    [CHANGE] Correccion en border radius

--- lib/app/widgets/slidable_item/slidable_item_widget.dart
+++ lib/app/widgets/slidable_item/slidable_item_widget.dart
@@ -18,7 +18,7 @@ class SlidableItemWidget extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.only(right: 10),
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(15),
+          borderRadius: BorderRadius.circular(10),
           child: Material(
             color: backgroundColor,
             child: InkWell(
